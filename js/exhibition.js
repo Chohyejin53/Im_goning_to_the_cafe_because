89a2a5b45c1ff66b9929553c36d2e088812098ce
Chohyejin53 <hyejin2338@gmail.com>
2021-04-28 02:15:58 +0900

    210427

---
+++ js/exhibition.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.multiple_slider').bxSlider({
+        minSlides:2,
+        maxSlides:5,
+        slideWidth:200,
+        slideMargin:30,
+        captions:false,
+        moveSlides:1,
+        pager:false,
+        controls:true,
+        
+    });
+});
+
