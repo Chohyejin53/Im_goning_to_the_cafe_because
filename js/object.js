0b2c94b853bf2eafec7a7a3778b8c4304a4315e7
Chohyejin53 <hyejin2338@gmail.com>
2021-05-02 04:10:49 +0900

    210502

--- js/object.js
+++ js/object.js
@@ -1,49 +1,135 @@
 
-//색 불러오기
-var colorUrl = "https://api.harvardartmuseums.org/color";
-colorUrl += "?apikey=a35af484-c468-4ceb-8054-5aa044a7f8b6";
-colorUrl += "&size=100";
-console.log("url: " + colorUrl);
+// //색 불러오기
+// var colorUrl = "https://api.harvardartmuseums.org/color";
+// colorUrl += "?apikey=a35af484-c468-4ceb-8054-5aa044a7f8b6";
+// colorUrl += "&size=100";
+// console.log("url: " + colorUrl);
     
-    $.ajax({
-        type: "GET",
-        url: colorUrl,
-        dataType: "json",
-        async: false,
-        success: function (data) {
-            console.log(data);
-            var records = data.records;
-            var info = data.info;
-            var hexCode = [];
-            var colorData = [];
+//     $.ajax({
+//         type: "GET",
+//         url: colorUrl,
+//         dataType: "json",
+//         async: false,
+//         success: function (data) {
+//             console.log(data);
+//             var records = data.records;
+//             var info = data.info;
+//             var hexCode = [];
+//             var colorData = [];
 
-            //sort해서 hex기준 오름차순으로 정렬맞춰주기
-            records.sort(function (a, b) { 
-                return a.hex < b.hex ? -1 : a.hex > b.hex ? 1 : 0;  
-            });
+//             //sort해서 hex기준 오름차순으로 정렬맞춰주기
+//             records.sort(function (a, b) { 
+//                 return a.hex < b.hex ? -1 : a.hex > b.hex ? 1 : 0;  
+//             });
 
-                for(var i = 0; i < records.length; i++) {
-                    hexCode[i] = data.records[i].hex;
-                    colorData[i] = 1;  // chart.js에 색 비율 1:1로 지정하기 위해서
-                    // console.log(hexCode);
+//                 for(var i = 0; i < records.length; i++) {
+//                     hexCode[i] = data.records[i].hex;
+//                     colorData[i] = 1;  // chart.js에 색 비율 1:1로 지정하기 위해서
+//                     // console.log(hexCode);
                     
-                };
-                console.log(hexCode);
-                console.log(colorData);
+//                 };
+//                 console.log(hexCode);
+//                 console.log(colorData);
 
-                //chart.js에 색 변경하기
-                var data = { 
-                    labels: hexCode, 
-                    datasets: [ { 
-                        data: colorData, 
-                        backgroundColor: hexCode, 
-                        // data: colorData, 
-                        // backgroundColor: arr, 
-                        borderWidth: 1, 
-                        // label: "Dataset 1" 
-                        }
-                    ] 
-                };
+//                 //chart.js에 색 변경하기
+//                 var data = { 
+//                     labels: hexCode, 
+//                     datasets: [ { 
+//                         data: colorData, 
+//                         backgroundColor: hexCode, 
+//                         // data: colorData, 
+//                         // backgroundColor: arr, 
+//                         borderWidth: 0, 
+//                         // label: "Dataset 1" 
+//                         }
+//                     ] 
+//                 };
+                
+//                 var config = {
+//                     type: 'pie',
+//                     data: data,
+//                     options: {
+//                         responsive: true,
+//                         plugins: {
+//                             legend: false,
+//                             title: {
+//                                 display: false,
+//                                 text: 'Chart.js Pie Chart'
+//                             },
+//                             tooltip: {
+//                                 enabled: false
+//                             }
+//                         },
+//                         onClick: chartClickEvent
+//                     },
+//                 };
+                
+//                 var myChart = new Chart(
+//                     document.getElementById('myChart'),
+//                     config
+//                 );
+
+                //컬러 인코딩 후 컬러 코드 사용하기
+                // for(var i = 0; i <hexCode.length; i++) {
+                //     var encodeColor = encodeURIComponent(hexCode[i]);  //컬러코드 인코딩
+                    var objectUrl = "https://api.harvardartmuseums.org/object";
+                    objectUrl += "?color=any";
+                    objectUrl += "&apikey=a35af484-c468-4ceb-8054-5aa044a7f8b6";
+                    objectUrl += "&size=100";
+                    // console.log("color: " + encodeColor);
+                    console.log("url: " + objectUrl);
+
+                    $.ajax({
+                        type: "GET",
+                        url: objectUrl,
+                        dataType: "json",
+                        async: false,
+                        success: function (objData) {
+                            console.log(objData);
+                            var objRecords = objData.records;
+                            var allObjColor = [];
+                            var objColor = [];
+                            var colorData = [];
+                            for(var i = 0; i < objRecords.length; i++) {
+                                for(var j = 0; j < objRecords[i].colors.length; j++) {
+                                    allObjColor[i] = objRecords[i].colors[j].color;
+                                    objImg[i] = objRecords[i].url;
+
+                                    
+                                };
+                                
+                            };
+                            console.log(objColor);
+                            console.log(objImg);
+
+
+                            //중복되는 색 제거 (마지막에 오기)
+                            objColor = allObjColor.filter(function(item, pos, self) {
+                                return self.indexOf(item) == pos;
+                            });
+                            
+                            for(var i= 0; i < objColor.length; i++) {
+                                colorData[i] = 1;  // chart.js에 색 비율 1:1로 지정하기 위해서
+                            }
+                            
+                            //sort해서 hex기준 오름차순으로 정렬맞춰주기 
+                            objColor.sort(function (a, b) { 
+                                return a.hex < b.hex ? -1 : a.hex > b.hex ? 1 : 0;  
+                            });  
+
+                            //chart.js에 색 변경하기
+                            var data = { 
+                            labels: objColor, 
+                            datasets: [ { 
+                                data: colorData, 
+                                backgroundColor: objColor, 
+                                // data: colorData, 
+                                // backgroundColor: arr, 
+                                borderWidth: 0, 
+                                // label: "Dataset 1" 
+                                }
+                                ] 
+                            };
                 
                 var config = {
                     type: 'pie',
@@ -69,23 +155,29 @@ console.log("url: " + colorUrl);
                     config
                 );
 
+                
+                        },
+                        error: function (request, status, error) {
+                            console.log("code:" + request.status);
+                            console.log("message:" + request.responseText);
+                            console.log("error:" + error);
+                        }
+                    });
 
-        },
-        error: function (request, status, error) {
-            console.log("code:" + request.status);
-            console.log("message:" + request.responseText);
-            console.log("error:" + error);
-        }
-    });        
+                // }
+                
+
+
+    //     },
+    //     error: function (request, status, error) {
+    //         console.log("code:" + request.status);
+    //         console.log("message:" + request.responseText);
+    //         console.log("error:" + error);
+    //     }
+    // });        
         
         
-//컬러 인코딩 후 컬러 코드 사용하기
-var encodeColor = encodeURIComponent("#c8af96");  //컬러코드 인코딩
-var url = "https://api.harvardartmuseums.org/object";
-url += "?color=" + encodeColor;
-url += "&apikey=a35af484-c468-4ceb-8054-5aa044a7f8b6";
-console.log("color: " + encodeColor);
-console.log("url: " + url);
+
 
     //color 코드 넣기
     // var arr = [];
