373a92756ecbd1584e74bdbdabf321a782a7a40e
Chohyejin53 <hyejin2338@gmail.com>
2021-05-03 14:01:59 +0900

    210503

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
+//메인 로고 작품 이미지
+
+var mainImgUrl = "https://api.harvardartmuseums.org/image";
+mainImgUrl += "?apikey=a35af484-c468-4ceb-8054-5aa044a7f8b6";
+mainImgUrl += "&size=100";
+console.log("url: " + mainImgUrl);
+
 $.ajax({
     type: "GET",
-    url: "https://api.harvardartmuseums.org/object?color/34838386&century/37525806&apikey=a35af484-c468-4ceb-8054-5aa044a7f8b6",
+    url: mainImgUrl,
     dataType: "json",
     async: false,
     success: function (data) {
-        console.log(data.records[0].images);
-        $(".img_obj").append("<img src ='" + data.records[0].images+ "' />");
+        console.log(data);
+        
     },
     error: function (request, status, error) {
         console.log("code:" + request.status);
@@ -15,7 +22,30 @@ $.ajax({
 });
 
 
-// var random = Math.floor(Math.random() * $('.item').length);
-// $('.item').hide().eq(random).show();
 
-// https://api.harvardartmuseums.org/object?color=beige
+
+
+
+
+
+// $.ajax({
+//     type: "GET",
+//     url: "https://api.harvardartmuseums.org/object?color/34838386&century/37525806&apikey=a35af484-c468-4ceb-8054-5aa044a7f8b6",
+//     dataType: "json",
+//     async: false,
+//     success: function (data) {
+//         console.log(data.records[0].images);
+//         $(".img_obj").append("<img src ='" + data.records[0].images+ "' />");
+//     },
+//     error: function (request, status, error) {
+//         console.log("code:" + request.status);
+//         console.log("message:" + request.responseText);
+//         console.log("error:" + error);
+//     }
+// });
+
+
+// // var random = Math.floor(Math.random() * $('.item').length);
+// // $('.item').hide().eq(random).show();
+
+// // https://api.harvardartmuseums.org/object?color=beige
