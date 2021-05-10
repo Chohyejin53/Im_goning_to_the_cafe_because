a530c8f5a4c87b31e900bcb693dd349f1a9c662b
Chohyejin53 <hyejin2338@gmail.com>
2021-05-10 18:55:54 +0900

    210510

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-$( window ).scroll( function() {
-	if ( $( this ).scrollTop() > 200 ) {
-		$( '.top' ).fadeIn();
+//스크롤바 내렸을떄, 메뉴바 고정
+var headerHeight = $('#header_wrap').height();
+$(window).scroll( function() {
+	if ( $( this ).scrollTop() > 600 ) {
+		$('#header_wrap').fadeIn();
+        $('#header_wrap').css('position','fixed');
+        $('content_wrap','footer_wrap').css('margin-top', headerHeight); 
 	} else {
-		$( '.top' ).fadeOut();
+		$('#header_wrap').fadeOut();
 	}
-} );
+});
+
+//메인비주얼 전체화면 
+var visualHeight = $(window).height();
+$('.visual_img').css('height',visualHeight);
