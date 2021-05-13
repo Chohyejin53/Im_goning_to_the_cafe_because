001749561011d62aa3099ace45279432bea0fa40
Chohyejin53 <hyejin2338@gmail.com>
2021-05-13 16:28:29 +0900

    210513

--- js/maps.js
+++ js/maps.js
@@ -1,6 +1,25 @@
-// api키 : db59e351dc7a480f1b625120d3f9f2d9
+api키 : db59e351dc7a480f1b625120d3f9f2d9
 
-// SDK 초기화
-Kakao.init('db59e351dc7a480f1b625120d3f9f2d9');
+//지도불러오기
+var container = document.getElementById('map');
+	var options = {
+		center: new kakao.maps.LatLng(37.45240609232767, 126.69969111326523),
+		level: 3
+	};
 
+	var map = new kakao.maps.Map(container, options);
+
+// 마커 생성위치
+var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
+
+// 마커 생성
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 지도위 마커 표시
+marker.setMap(map);
+
+// 마커 제거 코드
+// marker.setMap(null);    
 
