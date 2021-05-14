e23134643538f82626f9451f9f68162d0317d3a7
Chohyejin53 <hyejin2338@gmail.com>
2021-05-14 17:21:51 +0900

    210514

--- js/study.js
+++ js/study.js
@@ -1,10 +1,3 @@
-// 리스트 담을 배열
-var starList = [];
-var twosomeList = [];
-var ediyaList = [];
-var hollisList = [];
-var cafeIdx = 1;
-
 // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
 var infowindow = new kakao.maps.InfoWindow({zIndex:1});
 
@@ -28,27 +21,51 @@ ps.keywordSearch('인천 투썸플레이스', placesSearchCB);
 ps.keywordSearch('인천 이디야', placesSearchCB); 
 ps.keywordSearch('인천 할리스', placesSearchCB); 
 
-function checkCafeIdx(cafeIdx, data){
-    switch (cafeIdx) {
-        case 1: for(var i=0; i < data.length; i++) {
-                    starList.push(data[i]);
+
+function insertHTML(list, place) {
+    
+    switch (place) {
+        case "star": 
+                // console.log("starList: " + JSON.stringify(list));
+                var starPosition = [];
+                for (var i=0; i < list.length; i++) {
+                    starPosition.push(new kakao.maps.LatLng(list[i].y, list[i].x));
                 }
+                console.log(starPosition);
+
+            break;
+        case "twosome": 
+                // console.log("twosomeList: " + JSON.stringify(list));
+                var twosomePosition = [];
                 
-            break;
-        case 2: for(var i=0; i < data.length; i++) {
-                    twosomeList.push(data);
+                for (var i=0; i < list.length; i++) {
+                    twosomePosition.push(new kakao.maps.LatLng(list[i].y, list[i].x));
                 }
+                console.log(twosomePosition);
             break;
-        case 3: for(var i=0; i < data.length; i++) {
-                    ediyaList.push(data);
+        case "ediya": 
+                // console.log("ediyaList: " + JSON.stringify(list));
+                var eidyaPosition = [];
+                
+                for (var i=0; i < list.length; i++) {
+                    eidyaPosition.push(new kakao.maps.LatLng(list[i].y, list[i].x));
                 }
+                console.log(eidyaPosition);
             break;
-        case 4: for(var i=0; i < data.length; i++) {
-                    hollisList.push(data);
+        case "hollis": 
+                // console.log("hollisList: " + JSON.stringify(list));
+                var hollisPosition = [];
+                
+                for (var i=0; i < list.length; i++) {
+                    hollisPosition.push(new kakao.maps.LatLng(list[i].y, list[i].x));
                 }
+                console.log(hollisPosition);
             break;
+
+            
     }
 }
+
 
 
 
@@ -56,43 +73,48 @@ function checkCafeIdx(cafeIdx, data){
 function placesSearchCB (data, status, pagination) {
     if (status === kakao.maps.services.Status.OK) {
 
-        checkCafeIdx(cafeIdx, data);
+        // // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // // LatLngBounds 객체에 좌표를 추가합니다
+        // var bounds = new kakao.maps.LatLngBounds();
+        // console.log(data);
+        // for (var i=0; i<data.length; i++) {
+        //     displayMarker(data[i]);    
+        //     bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        // }    
 
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        // LatLngBounds 객체에 좌표를 추가합니다
-        var bounds = new kakao.maps.LatLngBounds();
-        console.log(data);
-        for (var i=0; i<data.length; i++) {
-            displayMarker(data[i]);    
-            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-        }       
+            var placeNM = data[0].place_name;
 
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-        map.setBounds(bounds);
+            if (placeNM.indexOf('스타벅스') > -1) {
+                insertHTML(data, "star");
+            } else if (placeNM.indexOf('투썸') > -1) {
+                insertHTML(data, "twosome");
+            } else if (placeNM.indexOf('이디야') > -1) {
+                insertHTML(data, "ediya");
+            } else if (placeNM.indexOf('할리스') > -1) {
+                insertHTML(data, "hollis");
 
-        cafeIdx++;
+            }
+
+        // // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        // map.setBounds(bounds);
+
     } 
 }
-// console.log(starList);
-// console.log(twosomeList);
-// console.log(ediyaList);
-// console.log(hollisList);
 
-// 지도에 마커를 표시하는 함수입니다
-function displayMarker(place) {
+
+// // 지도에 마커를 표시하는 함수입니다
+// function displayMarker(place) {
     
-    // 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(place.y, place.x) 
-    });
+//     // 마커를 생성하고 지도에 표시합니다
+//     var marker = new kakao.maps.Marker({
+//         map: map,
+//         position: new kakao.maps.LatLng(place.y, place.x) 
+//     });
 
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-        infowindow.open(map, marker);
-    });
-}
-
-// var latlng = new kakao.maps.LatLng( );
+//     // 마커에 클릭이벤트를 등록합니다
+//     kakao.maps.event.addListener(marker, 'click', function() {
+//         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+//         infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+//         infowindow.open(map, marker);
+//     });
+// }
