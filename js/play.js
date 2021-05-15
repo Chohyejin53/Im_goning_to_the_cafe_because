9ff6af03ffaea13f7a5ccb0c6d701be774dd07d2
Chohyejin53 <hyejin2338@gmail.com>
2021-05-15 16:37:21 +0900

    210515

--- js/play.js
+++ js/play.js
@@ -5,7 +5,7 @@
     
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
         mapOption = {
-            center: new kakao.maps.LatLng(37.45240609232767, 126.69969111326523), // 지도의 중심좌표
+            center: new kakao.maps.LatLng(37.469221, 126.603234), // 지도의 중심좌표
             level: 6 // 지도의 확대 레벨
         };  
     
@@ -17,7 +17,9 @@
     
     // 키워드로 장소를 검색합니다
     ps.keywordSearch('인천 카페', placesSearchCB); 
-    
+    ps.keywordSearch('부평 테마 카페', placesSearchCB);
+    ps.keywordSearch('구월동 감성카페', placesSearchCB);
+
     // 키워드 검색 완료 시 호출되는 콜백함수 입니다
     function placesSearchCB (data, status, pagination) {
         if (status === kakao.maps.services.Status.OK) {
@@ -32,7 +34,7 @@
                 bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
             }       
     
-            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+            // // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
             map.setBounds(bounds);
     
             // 완료후 맛집 리스트 표출(json을 text개체로)
